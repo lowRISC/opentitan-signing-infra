lowRISC/opentitan

[hsmtool] Add support for kdf secrets.
KDF secrets are used to derive OpenTitan `TEST_UNLOCK` and `TEST_EXIT`
tokens in the provisioning infrastructure. This change adds support for
import/export operations in plaintext mode. A follow up commit will add
support for wrapped keys, as well as pkcs12 container support to be able
to load the secrets into USB tokens.

Signed-off-by: Miguel Osorio <miguelosorio@google.com>

sw/host/hsmtool/src/commands/kdf/export.rs
@@ -1,0 +1,102 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{bail, Result};
+use cryptoki::object::ObjectHandle;
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::path::PathBuf;
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::{AttributeMap, AttributeType, KeyType, ObjectClass};
+use crate::util::helper;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Export {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    /// Wrap the exported key using a wrapping key.
+    #[arg(long)]
+    wrap: Option<String>,
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+impl Export {
+    fn export(&self, session: &Session, object: ObjectHandle) -> Result<()> {
+        let map = AttributeMap::from_object(session, object)?;
+
+        let class: ObjectClass = map
+            .get(&AttributeType::Class)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a class attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        let key_type: KeyType = map
+            .get(&AttributeType::KeyType)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a key type attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if class != ObjectClass::SecretKey || key_type != KeyType::GenericSecret {
+            Err(HsmError::KeyError("Key is not a secret key".into()))?;
+        }
+
+        let sensitive: bool = map
+            .get(&AttributeType::Sensitive)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a sensitive attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if sensitive {
+            Err(HsmError::KeyError("Key is marked as sensitive".into()))?;
+        }
+
+        let extractable: bool = map
+            .get(&AttributeType::Extractable)
+            .ok_or_else(|| HsmError::KeyError("Key does not have an extractable attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if !extractable {
+            Err(HsmError::KeyError("Key is not extractable".into()))?;
+        }
+
+        let key: Vec<u8> = map
+            .get(&AttributeType::Value)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a value attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+
+        if let Some(output) = &self.output {
+            helper::write_file(output, &key)?;
+        }
+        Ok(())
+    }
+
+    fn wrap_key(&self, _session: &Session, _object: ObjectHandle) -> Result<()> {
+        bail!("Kdf key export by wrapping is not supported yet!");
+    }
+}
+
+#[typetag::serde(name = "kdf-export")]
+impl Dispatch for Export {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        let attrs = helper::search_spec(self.id.as_deref(), self.label.as_deref())?;
+        let object = helper::find_one_object(session, attrs.as_slice())?;
+        if self.wrap.is_some() {
+            self.wrap_key(session, object)?;
+        } else {
+            self.export(session, object)?;
+        }
+        Ok(Box::<BasicResult>::default())
+    }
+}

sw/host/hsmtool/src/commands/kdf/import.rs
@@ -1,0 +1,81 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{bail, Result};
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
+use crate::util::helper;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Import {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    #[arg(long)]
+    attributes: Option<AttributeMap>,
+    /// Unwrap the imported key with a wrapping key.
+    #[arg(long)]
+    unwrap: Option<String>,
+    filename: PathBuf,
+}
+
+impl Import {
+    const ATTRIBUTES: &'static str = r#"{
+        "CKA_TOKEN": true,
+        "CKA_CLASS": "CKO_SECRET_KEY",
+        "CKA_KEY_TYPE": "CKK_GENERIC_SECRET",
+        "CKA_DERIVE": true,
+    }"#;
+
+    fn unwrap_key(&self, _session: &Session, _template: &AttributeMap) -> Result<()> {
+        bail!("Kdf key import by unwrapping is not supported yet!");
+    }
+}
+
+#[typetag::serde(name = "kdf-import")]
+impl Dispatch for Import {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        helper::no_object_exists(session, self.id.as_deref(), self.label.as_deref())?;
+
+        let mut attributes = AttributeMap::from_str(Self::ATTRIBUTES).expect("error in ATTRIBUTES");
+        let id = AttrData::Str(self.id.as_ref().cloned().unwrap_or_else(helper::random_id));
+        let result = Box::new(BasicResult {
+            success: true,
+            id: id.clone(),
+            label: AttrData::Str(self.label.as_ref().cloned().unwrap_or_default()),
+            value: None,
+            error: None,
+        });
+
+        attributes.insert(AttributeType::Id, id.clone());
+        attributes.insert(AttributeType::Label, result.label.clone());
+        if let Some(tpl) = &self.attributes {
+            attributes.merge(tpl.clone());
+        }
+
+        if self.unwrap.is_some() {
+            self.unwrap_key(session, &attributes)?;
+        } else {
+            let key = helper::read_file(&self.filename)?;
+            attributes.insert(AttributeType::Value, AttrData::from(key.as_slice()));
+            let _secretkey = session.create_object(&attributes.to_vec()?)?;
+        }
+        Ok(result)
+    }
+}

lowRISC/opentitan-signing-infra

(cherry picked from commit 94139bd56c962a17972b98facda1fc66d22174b6)

hsmtool/src/commands/kdf/export.rs
@@ -1,0 +1,104 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::any::Any;
+use std::fs;
+use std::path::PathBuf;
+
+use anyhow::{Result, bail};
+use cryptoki::object::ObjectHandle;
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::{AttributeMap, AttributeType, KeyType, ObjectClass};
+use crate::util::helper;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Export {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    /// Wrap the exported key using a wrapping key.
+    #[arg(long)]
+    wrap: Option<String>,
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+impl Export {
+    fn export(&self, session: &Session, object: ObjectHandle) -> Result<()> {
+        let map = AttributeMap::from_object(session, object)?;
+
+        let class: ObjectClass = map
+            .get(&AttributeType::Class)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a class attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        let key_type: KeyType = map
+            .get(&AttributeType::KeyType)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a key type attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if class != ObjectClass::SecretKey || key_type != KeyType::GenericSecret {
+            Err(HsmError::KeyError("Key is not a secret key".into()))?;
+        }
+
+        let sensitive: bool = map
+            .get(&AttributeType::Sensitive)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a sensitive attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if sensitive {
+            Err(HsmError::KeyError("Key is marked as sensitive".into()))?;
+        }
+
+        let extractable: bool = map
+            .get(&AttributeType::Extractable)
+            .ok_or_else(|| HsmError::KeyError("Key does not have an extractable attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+        if !extractable {
+            Err(HsmError::KeyError("Key is not extractable".into()))?;
+        }
+
+        let key: Vec<u8> = map
+            .get(&AttributeType::Value)
+            .ok_or_else(|| HsmError::KeyError("Key does not have a value attribute".into()))?
+            .try_into()
+            .map_err(HsmError::AttributeError)?;
+
+        if let Some(output) = &self.output {
+            fs::write(output, &key)?;
+        }
+        Ok(())
+    }
+
+    fn wrap_key(&self, _session: &Session, _object: ObjectHandle) -> Result<()> {
+        bail!("Kdf key export by wrapping is not supported yet!");
+    }
+}
+
+#[typetag::serde(name = "kdf-export")]
+impl Dispatch for Export {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        let attrs = helper::search_spec(self.id.as_deref(), self.label.as_deref())?;
+        let object = helper::find_one_object(session, attrs.as_slice())?;
+        if self.wrap.is_some() {
+            self.wrap_key(session, object)?;
+        } else {
+            self.export(session, object)?;
+        }
+        Ok(Box::<BasicResult>::default())
+    }
+}

hsmtool/src/commands/kdf/import.rs
@@ -1,0 +1,83 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::any::Any;
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use anyhow::{Result, bail};
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
+use crate::util::helper;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Import {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    #[arg(long)]
+    attributes: Option<AttributeMap>,
+    /// Unwrap the imported key with a wrapping key.
+    #[arg(long)]
+    unwrap: Option<String>,
+    filename: PathBuf,
+}
+
+impl Import {
+    const ATTRIBUTES: &'static str = r#"{
+        "CKA_TOKEN": true,
+        "CKA_CLASS": "CKO_SECRET_KEY",
+        "CKA_KEY_TYPE": "CKK_GENERIC_SECRET",
+        "CKA_DERIVE": true,
+    }"#;
+
+    fn unwrap_key(&self, _session: &Session, _template: &AttributeMap) -> Result<()> {
+        bail!("Kdf key import by unwrapping is not supported yet!");
+    }
+}
+
+#[typetag::serde(name = "kdf-import")]
+impl Dispatch for Import {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        helper::no_object_exists(session, self.id.as_deref(), self.label.as_deref())?;
+
+        let mut attributes = AttributeMap::from_str(Self::ATTRIBUTES).expect("error in ATTRIBUTES");
+        let id = AttrData::Str(self.id.as_ref().cloned().unwrap_or_else(helper::random_id));
+        let result = Box::new(BasicResult {
+            success: true,
+            id: id.clone(),
+            label: AttrData::Str(self.label.as_ref().cloned().unwrap_or_default()),
+            value: None,
+            error: None,
+        });
+
+        attributes.insert(AttributeType::Id, id.clone());
+        attributes.insert(AttributeType::Label, result.label.clone());
+        if let Some(tpl) = &self.attributes {
+            attributes.merge(tpl.clone());
+        }
+
+        if self.unwrap.is_some() {
+            self.unwrap_key(session, &attributes)?;
+        } else {
+            let key = fs::read(&self.filename)?;
+            attributes.insert(AttributeType::Value, AttrData::from(key.as_slice()));
+            let _secretkey = session.create_object(&attributes.to_vec()?)?;
+        }
+        Ok(result)
+    }
+}