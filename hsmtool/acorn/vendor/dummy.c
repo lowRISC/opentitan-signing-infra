97c95092b7ec02f47b85236d304c846bb0416317
Alex Jones <alex.jones@lowrisc.org>
2026-07-23 18:00:58 +0100

    [bazel] Add dummy C file to acorn header bindgen build
    In OpenTitan we carry a custom rules_rust patch to allow
    `rust_bindgen_library` to be used without any static library defined -
    there is only one such target for the `hsmtool` build, so it is easier
    just to drop the patch and carry a dummy file here, to increase
    compatibility with upstream rules_rust.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

---
+++ hsmtool/acorn/vendor/dummy.c
@@ -0,0 +1,6 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+/* Dummy file to force Bazel rules_rust_bindgen static library creation.
+ * Required to not carry a separate patch to rules_rust_bindgen. */
