d5f203a2d5240e5febf337d2e111af619d30bdfc
Alex Jones <alex.jones@lowrisc.org>
2026-08-11 13:34:16 +0100

    [hsmtool] Source bash for scripts from `usr/bin/env`
    Increase compatibility with a variety of environments, such as e.g. with
    NixOS systems, which do not by default have a `/usr/bin/bash`.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- hsmtool/tests/ot_hsm_runner.sh
+++ hsmtool/tests/ot_hsm_runner.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
 # Copyright lowRISC contributors (OpenTitan project).
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
